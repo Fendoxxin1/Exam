cff9016d30397856283fac02fd8a8bc434078bd2
Yangi_Nom <odilbek5060@gmail.com>
2025-03-25 15:27:51 +0500

    ozgarishlar kirtildi

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("kommanda", "root", "Odil5060", {
+const db = new Sequelize("n17", "root", "Odil5060", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
