d1081f6a7bf9b13b3b6abd464c38534b56e74a34
Fendoxxin1 <fendoxxindehlaviy@gmail.com>
2025-03-26 18:01:34 +0500

    refresh token qo'shildi

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
 const { Sequelize } = require("sequelize");
 
-
 // const db = new Sequelize("n17", "root", "Odil5060", {
 //   host: "localhost",
 //   dialect: "mysql",
 //   logging: false,
 // });
-
 
 // const db = new Sequelize("az", "root", "1234", {
 //   host: "localhost",
@@ -17,6 +15,7 @@ const { Sequelize } = require("sequelize");
 const db = new Sequelize("exam3", "root", "billybutcher1", {
   host: "localhost",
   dialect: "mysql",
+  logging: false,
 });
 
 async function connectDb() {
